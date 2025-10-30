cf928bc8edcfa7430bb7012a0ce2a12f9f7fe439
sai chaitanya <ankemsaichaitanya@gmail.com>
2025-10-30 21:18:18 +0530

    Add TDD test for Weather App

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// ✅ Basic test to check if title renders
+test('renders Weather App heading', () => {
+    render(<App />);
+    const titleElement = screen.getByText(/hello App/i);
+    expect(titleElement).toBeInTheDocument();
+});
